4439d276deac5636ddd77f73a3e46d666e65b0db
Chris Tweney <chris@media.berkeley.edu>
2014-09-25 17:35:14 -0700

    Delete LineFactory; tweak tests

--- spec/load_tests/google_charts_json_converter_spec.rb
+++ spec/load_tests/google_charts_json_converter_spec.rb
@@ -8,11 +8,15 @@ describe LoadTests::GoogleChartsJsonConverter do
   it {
     puts "Google Charts JSON Data: #{subject}"
     expect(subject).to be
+    expect(subject[:highest_cpu][0][0]).to eq 1411394598
     expect(subject[:highest_cpu][0][1]).to eq 252.2
     expect(subject[:highest_cpu][0][2]).to eq "1: 252.2%"
+    expect(subject[:lowest_memory][0][0]).to eq 1411394598
     expect(subject[:lowest_memory][0][1]).to eq 3852.1
+    expect(subject[:session_mean][0][0]).to eq 1411394598
     expect(subject[:session_mean][0][1]).to eq 95596.7
     expect(subject[:session_mean][0][3]).to eq 113181.9
+    expect(subject[:error_count][0][0]).to eq 1411394598
     expect(subject[:error_count][0][1]).to eq 15
   }
 
